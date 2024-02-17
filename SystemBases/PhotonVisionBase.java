fc9622ee4d5e86cdcf61e194353dcfc7e25e4380
Duel M <dueromoods@gmail.com>
2024-02-17 16:28:19 -0500

    VISION WORKS

---
+++ SystemBases/PhotonVisionBase.java
@@ -0,0 +1,59 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.SyncedLibraries.SystemBases;
+
+import java.util.List;
+import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class PhotonVisionBase extends SubsystemBase {
+  public PhotonCamera camera;
+  public boolean hasTarget = false;
+  public PhotonPipelineResult latestResult;
+  public List<PhotonTrackedTarget> targets;
+  public PhotonTrackedTarget mainTarget;
+  public double[] targetXYAngles = new double[2];
+
+  public PhotonVisionBase(PhotonCamera camera) {
+    camera.setPipelineIndex(0);
+    camera.setDriverMode(false);
+    this.camera = camera;
+  }
+
+  @Override
+  public void periodic() {
+    double x = 0;
+    double y = 0;
+    latestResult = camera.getLatestResult();
+    targets = latestResult.getTargets();
+    hasTarget = latestResult.hasTargets();
+    if (hasTarget) {
+      mainTarget = latestResult.getBestTarget();
+      x = mainTarget.getYaw();
+      y = mainTarget.getPitch();
+
+
+      targetXYAngles[0] = x;
+      targetXYAngles[1] = y;
+    }
+
+    SmartDashboard.putNumber("TargetX", x);
+    SmartDashboard.putNumber("TargetY", y);
+  }
+
+  protected PhotonTrackedTarget containsTarget(List<PhotonTrackedTarget> targets, int... fiducialIds) {
+    for (int i = 0; i < targets.size(); i++) {
+      for (int j = 0; j < fiducialIds.length; j++) {
+        if (targets.get(i).getFiducialId() == fiducialIds[j]) {
+          return targets.get(i);
+        }
+      }
+    }
+    return null;
+  }
+}
