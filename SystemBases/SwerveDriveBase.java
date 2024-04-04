e2755dbecae0d0da2bfbc884b5e104d17f894175
Duel M <dueromoods@gmail.com>
2024-04-03 20:06:05 -0400

    Continued adding swerve functionality

--- SystemBases/SwerveDriveBase.java
+++ SystemBases/SwerveDriveBase.java
@@ -5,11 +5,11 @@
 package frc.robot.SyncedLibraries.SystemBases;
 
 import com.revrobotics.CANSparkBase.ControlType;
+import com.ctre.phoenix6.hardware.CANcoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkPIDController;
-
-import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 /** For info about positions and standards, read comment inside */
@@ -25,51 +25,160 @@ public class SwerveDriveBase extends SubsystemBase {
    * 
    * For directions, 0 is forward,
    * positive is clockwise, negative is counter-clockwise
-   * Pi is reverse
-   * In radians
+   * 180 is reverse
    */
+
+  private SwerveModule[] modules;
+
+  /**
+   * Represents a swerve drive base that consists of multiple swerve modules.
+   * <p>
+   * All arrays must be of length 4. For more information see comment inside.
+   * 
+   * @param turnMotors         The motors that control the turning of the
+   *                           wheels.
+   * @param turnEncoders       The encoders that measure the turning of the
+   *                           wheels.
+   * @param absoluteEncoders   The encoders that measure the absolute turning of
+   *                           the wheels.
+   * @param absoluteTurnOffset The initial positions of the turn motors in
+   *                           degrees.
+   * @param driveMotors        The motors that control the driving of the
+   *                           wheels.
+   * @param maxDriveSpeed      The maximum speed of the drive motors.
+   * @param amps               The current limits for the motors in the format
+   *                           of [turn, drive].
+   * @param turnP              The proportional gain for the turn PID.
+   * @param turnI              The integral gain for the turn PID.
+   * @param turnD              The derivative gain for the turn PID.
+   */
+  public SwerveDriveBase(CANSparkMax[] turnMotors, CANcoder[] absoluteEncoders,
+      double[] absoluteTurnOffset, double turnMultiplier,
+      CANSparkMax[] driveMotors, double maxDriveSpeed, int[] amps,
+      double turnP, double turnI, double turnD) {
+
+    modules = new SwerveModule[4];
+
+    for (int i = 0; i < 4; i++) {
+      modules[i] = new SwerveModule(turnMotors[i], absoluteEncoders[i],
+          absoluteTurnOffset[i], turnMultiplier,
+          driveMotors[i], maxDriveSpeed, amps,
+          turnP, turnI, turnD);
+    }
+  }
 
   @Override
   public void periodic() {
+    for (SwerveModule module : modules) {
+      module.update();
+    }
   }
 
-  private class SwerveModule extends SubsystemBase {
+  /** @param direction angle in degrees from positive x direction */
+  public void update(double power, double direction, double rotation) {
+    double[] directions = new double[4];
+    double[] speeds = new double[4];
+    for (int i = 0; i < 4; i++) {
+      // TODO: Find what directions to turn to
+      directions[i] = 0;
+
+      // TODO: Find what speeds to drive at
+      speeds[i] = 0.5;
+
+      modules[i].setTurnPosition(directions[i]);
+      modules[i].setPower(speeds[i]);
+    }
+  }
+
+  public SwerveModule[] getModules() {
+    return modules;
+  }
+
+  public SwerveModule getModule(int index) {
+    return modules[index];
+  }
+
+  public void setDriveAmps(int amps) {
+    for (SwerveModule module : modules) {
+      module.setDriveAmps(amps);
+    }
+  }
+
+  public void setTurnAmps(int amps) {
+    for (SwerveModule module : modules) {
+      module.setTurnAmps(amps);
+    }
+  }
+
+  public void x_lock() {
+    for (int i = 0; i < 4; i++) {
+      modules[i].setTurnPosition((315 + i * 270) % 360);
+    }
+    brakeMode(true);
+  }
+
+  public void brakeMode(boolean brake) {
+    for (SwerveModule module : modules) {
+      module.setBrakeMode(brake);
+    }
+  }
+
+  private class SwerveModule {
     private CANSparkMax driveMotor;
     private CANSparkMax turnMotor;
     private RelativeEncoder turnEncoder;
     private RelativeEncoder driveEncoder;
     private SparkPIDController turnPID;
-    private SparkPIDController drivePID;
     private double targetTurnPosition;
-    private double targetDriveSpeed;
-    private boolean powerMode = false;
+    private double targetPower;
 
-    public SwerveModule(CANSparkMax driveMotor, CANSparkMax turnMotor, double maxDriveSpeed,
-        double turnP, double turnI, double turnD,
-        double driveP, double driveI, double driveD) {
+    /**
+     * Constructs a SwerveModule object with the specified parameters.
+     *
+     * @param turnMotor           the CANSparkMax object representing the turn motor
+     *                            of the swerve module
+     * @param absoluteEncoder     the CANcoder object representing the absolute
+     *                            encoder of the swerve module
+     * @param initialTurnPosition the initial position of the turn encoder
+     * @param turnMultiplier      the multiplier for the turn encoder (for gears)
+     * @param driveMotor          the CANSparkMax object representing the drive
+     *                            motor of the swerve module
+     * @param maxDriveSpeed       the maximum speed of the drive motor
+     * @param amps                an array of integers representing the current
+     *                            limits for the turn and drive motors
+     * @param turnP               the proportional gain for the turn motor's PID
+     *                            controller
+     * @param turnI               the integral gain for the turn motor's PID
+     *                            controller
+     * @param turnD               the derivative gain for the turn motor's PID
+     *                            controller
+     */
+    public SwerveModule(CANSparkMax turnMotor, CANcoder absoluteEncoder,
+        double initialTurnPosition, double turnMultiplier,
+        CANSparkMax driveMotor, double maxDriveSpeed, int[] amps,
+        double turnP, double turnI, double turnD) {
+
       this.driveMotor = driveMotor;
       this.turnMotor = turnMotor;
+
+      setDriveAmps(amps[1]);
+      this.driveEncoder = driveMotor.getEncoder();
+
+      setTurnAmps(amps[0]);
       this.turnEncoder = turnMotor.getEncoder();
+      this.turnEncoder.setPosition(
+          absoluteEncoder.getPosition().getValue() * 360 - initialTurnPosition);
+      this.turnEncoder.setPositionConversionFactor(turnMultiplier);
 
       turnPID = turnMotor.getPIDController();
       turnPID.setP(turnP);
       turnPID.setI(turnI);
       turnPID.setD(turnD);
       turnPID.setPositionPIDWrappingEnabled(true);
-      turnPID.setPositionPIDWrappingMinInput(-Math.PI);
-      turnPID.setPositionPIDWrappingMaxInput(Math.PI);
+      turnPID.setPositionPIDWrappingMinInput(-180);
+      turnPID.setPositionPIDWrappingMaxInput(180);
 
-      drivePID = driveMotor.getPIDController();
-      drivePID.setP(driveP);
-      drivePID.setI(driveI);
-      drivePID.setD(driveD);
-      setMaxDriveSpeed(maxDriveSpeed);
-    }
-
-    public void setMaxDriveSpeed(double speed) {
-      drivePID.setOutputRange(-speed, speed);
-      drivePID.setSmartMotionMaxVelocity(speed, 0);
-      drivePID.setIZone(speed * 0.1);
+      burn();
     }
 
     /** Burn flash on all motor controllers */
@@ -78,87 +187,78 @@ public class SwerveDriveBase extends SubsystemBase {
       driveMotor.burnFlash();
     }
 
-    /** Set speed from in rpm */
-    public void setDriveSpeed(double speed) {
-      targetDriveSpeed = speed;
-    }
-
-    /** Set position in radians */
+    /** Set position in degrees */
     public void setTurnPosition(double position) {
       targetTurnPosition = position;
     }
 
-    /** Returns in radians */
+    /** Returns in degrees */
     public double getTargetTurnPosition() {
       return targetTurnPosition;
     }
 
-    /** Returns in rpm */
-    public double getTargetDriveSpeed() {
-      return targetDriveSpeed;
+    public void setPower(double power) {
+      targetPower = power;
     }
 
-    /** Returns in radians */
+    /** Returns in degrees */
     public double getTurnPosition() {
       return turnEncoder.getPosition();
-    }
-
-    /** Returns in rpm */
-    public double getDriveSpeed() {
-      return driveMotor.getEncoder().getVelocity();
     }
 
     public RelativeEncoder getTurnEncoder() {
       return turnEncoder;
     }
 
-    /**
-     * Use for pushing or speed, will likely
-     * turn uncontrollably when driving
-     */
-    public void setPowerMode(boolean powerMode) {
-      this.powerMode = powerMode;
+    public RelativeEncoder getDriveEncoder() {
+      return driveEncoder;
     }
 
-    @Override
-    public void periodic() {
+    public void setTurnAmps(int amps) {
+      turnMotor.setSmartCurrentLimit(amps);
+    }
+
+    public void setDriveAmps(int amps) {
+      driveMotor.setSmartCurrentLimit(amps);
+    }
+
+    /** If value is greater than 180, it will be wrapped to -180 */
+    private double unWrapError() {
+      double error = getTargetTurnPosition() - getTurnPosition();
+      while (error > 180) {
+        error -= 360;
+      }
+      while (error < -180) {
+        error += 360;
+      }
+      return error;
+    }
+
+    public void setBrakeMode(boolean brake) {
+      driveMotor.setIdleMode(brake ? CANSparkMax.IdleMode.kBrake : CANSparkMax.IdleMode.kCoast);
+    }
+
+    /** CALL IN PERIODIC */
+    public void update() {
       /*
        * Find closest path to target angle
        * If error is greater than 90 degrees,
        * then face opposite direction
        * Reversing drive motor is handled by the cosine function in the drivePID
        */
-      double error = Math.abs(unWrapError());
-      if (error > Math.PI / 2) {
-        targetTurnPosition += Math.PI;
+      double position = unWrapError();
+      double error = Math.abs(position);
+      if (error > 180 / 2) {
+        targetTurnPosition += 180;
       }
       turnPID.setReference(targetTurnPosition, ControlType.kSmartMotion);
 
-      double cosErr = Math.cos(unWrapError());
+      double cosErr = (Math.cos(Units.degreesToRadians(position)));
       if (Math.abs(cosErr) < 0.1) {
         cosErr = 0;
       }
 
-      if (powerMode) {
-        // Go full power
-        drivePID.setReference(
-            cosErr, ControlType.kDutyCycle);
-      } else {
-        // Actually be smart about speed
-        drivePID.setReference(cosErr * targetDriveSpeed,
-            ControlType.kSmartVelocity);
-      }
-    }
-
-    /** If value is greater than Pi, it will be wrapped to -Pi */
-    private double unWrapError() {
-      double error = getTargetTurnPosition() - getTurnPosition();
-      if (error > Math.PI) {
-        error -= 2 * Math.PI;
-      } else if (error < -Math.PI) {
-        error += 2 * Math.PI;
-      }
-      return error;
+      driveMotor.set(cosErr * targetPower);
     }
   }
 }
