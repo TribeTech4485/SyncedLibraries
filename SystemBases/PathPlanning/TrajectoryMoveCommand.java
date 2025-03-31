a8fd574bc779f3cf2f85d5220713deb0f94e463a
Duel M <dueromoods@gmail.com>
2025-03-30 20:15:59 -0400

    Remove unused imports

--- SystemBases/PathPlanning/TrajectoryMoveCommand.java
+++ SystemBases/PathPlanning/TrajectoryMoveCommand.java
@@ -5,7 +5,6 @@ import static edu.wpi.first.units.Units.MetersPerSecondPerSecond;
 
 import edu.wpi.first.math.controller.HolonomicDriveController;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
